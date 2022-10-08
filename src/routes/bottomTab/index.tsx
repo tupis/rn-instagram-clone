fcbc18ff27a41ed9c28b8a8ce52b85fc48906a76
Tupis <joaoh.tupinamba@gmail.com>
2022-10-08 00:50:09 -0300

    feat: create tab navigator

--- src/routes/bottomTab/index.tsx
+++ src/routes/bottomTab/index.tsx
@@ -1,14 +1,12 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Feed from "../../screens/Feed";
 import Perfil from "../../screens/Perfil";
-import Reels from "../../screens/Reels";
 
 export default function Home() {
   const Tab = createBottomTabNavigator();
   return (
     <Tab.Navigator>
       <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Reels" component={Reels} />
       <Tab.Screen name="Perfil" component={Perfil} />
     </Tab.Navigator>
   );
