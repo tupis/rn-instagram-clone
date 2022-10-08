8c787a6d2ef3d33d8fbffa2769233318f2f1e590
Tupis <joaoh.tupinamba@gmail.com>
2022-10-08 00:44:31 -0300

    feat: create stack navigator

---
+++ src/routes/index.tsx
@@ -0,0 +1,47 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useState, useLayoutEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Login from "../screens/Login";
+import Register from "../screens/Register";
+import Home from "./bottomTab";
+
+export default function Routes() {
+  const Stack = createNativeStackNavigator();
+  const [isLogged, setIsLogged] = useState<boolean>(false);
+
+  const getToken = async () => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      setIsLogged(true);
+    } else {
+      setIsLogged(false);
+    }
+  };
+
+  useLayoutEffect(() => {
+    getToken();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={isLogged ? "Home" : "Login"}>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
