a6484e37d760d685743ee53c9ca1a8100fff13ad
Tupis <joaoh.tupinamba@gmail.com>
2022-10-08 15:30:39 -0300

    fix: receveive params navigation through desconstructor

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -1,9 +1,11 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-export default function Feed() {
+export default function Feed(props: any) {
+  const { navigation } = props;
   return (
     <View>
       <Text>Feed</Text>
+      <Button title="Login" onPress={() => navigation.navigate("Login")} />
     </View>
   );
 }
