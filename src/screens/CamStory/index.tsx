1f8fd540c7058d3ee6cd4027df1d5f1ee8d971f5
Tupis <joaoh.tupinamba@gmail.com>
2022-10-10 15:39:56 -0300

    feat: create function capture and display photo

--- src/screens/CamStory/index.tsx
+++ src/screens/CamStory/index.tsx
@@ -1,11 +1,22 @@
-import { Text, View, Button, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  SafeAreaView,
+  Modal,
+  Image,
+} from "react-native";
 import { Camera, CameraType } from "expo-camera";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { styles } from "./styles";
 
 export default function CamStory() {
+  const camRef = useRef<any>(null);
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [capturedPhoto, setCaputredPhoto] = useState<any>();
+  const [open, setOpen] = useState<any>(false);
 
   if (!permission) {
     return <View />;
@@ -29,15 +40,51 @@ export default function CamStory() {
     );
   }
 
+  async function takePicture() {
+    if (camRef) {
+      const data = await camRef.current.takePictureAsync();
+      setCaputredPhoto(data.uri);
+      setOpen(true);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+    <SafeAreaView style={styles.container}>
+      <Camera style={styles.camera} type={type} ref={camRef}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
             <Text style={styles.text}>Flip Camera</Text>
           </TouchableOpacity>
         </View>
       </Camera>
-    </View>
+
+      <TouchableOpacity onPress={takePicture} style={{ height: 100 }}>
+        <Text style={{ color: "white", alignSelf: "center" }}>TakePicture</Text>
+      </TouchableOpacity>
+
+      {capturedPhoto && (
+        <Modal animationType="slide" transparent={false} visible={open}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              margin: 20,
+            }}
+          >
+            <TouchableOpacity
+              style={{ margin: 20 }}
+              onPress={() => setOpen(false)}
+            >
+              <Text>Close</Text>
+            </TouchableOpacity>
+            <Image
+              style={{ width: "100%", height: 300, borderRadius: 20 }}
+              source={{ uri: `${capturedPhoto}` }}
+            />
+          </View>
+        </Modal>
+      )}
+    </SafeAreaView>
   );
 }
--- src/screens/CamStory/styles.ts
+++ src/screens/CamStory/styles.ts
@@ -4,6 +4,7 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    backgroundColor: "black",
   },
   camera: {
     flex: 1,
