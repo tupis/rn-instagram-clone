c9b2b7f339e7c26682c0e0b3dc58ce09fffcc0d0
Tupis <joaoh.tupinamba@gmail.com>
2022-10-14 00:27:26 -0300

    feat: create scrollview

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -1,6 +1,6 @@
-import { Text, Button } from "react-native";
 import Header from "../../components/Header";
-import { Container } from "./styles";
+import Post from "../../components/Post";
+import { Container, Scroll } from "./styles";
 
 export default function Feed(props: any): JSX.Element {
   const { navigation } = props;
@@ -8,8 +8,9 @@ export default function Feed(props: any): JSX.Element {
   return (
     <Container>
       <Header navigation={navigation} />
-      <Text>Feed</Text>
-      <Button title="Login" onPress={() => navigation.navigate("Login")} />
+      {/* <Scroll> */}
+      <Post />
+      {/* </Scroll> */}
     </Container>
   );
 }
--- src/screens/Feed/styles.ts
+++ src/screens/Feed/styles.ts
@@ -1,7 +1,12 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
   justify-content: center;
   align-items: center;
+`;
+
+export const Scroll = styled.ScrollView`
+  width: 100%;
+  height: undefined;
 `;
