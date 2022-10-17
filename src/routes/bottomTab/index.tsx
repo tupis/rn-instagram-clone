fcf305ae79f958508109c0efe4e9ca46fc646744
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-10-16 22:57:37 -0300

    feat: add icons to bottom navbar

--- src/routes/bottomTab/index.tsx
+++ src/routes/bottomTab/index.tsx
@@ -1,3 +1,4 @@
+import Icons from "react-native-vector-icons/Ionicons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Feed from "../../screens/Feed";
 import Perfil from "../../screens/Perfil";
@@ -5,7 +6,28 @@ import Perfil from "../../screens/Perfil";
 export default function Home(): JSX.Element {
   const Tab = createBottomTabNavigator();
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: string = "";
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          }
+          if (route.name === "Perfil") {
+            iconName = focused ? "person" : "person-outline";
+          }
+
+          focused ? (size += 5) : "";
+
+          color = "black";
+
+          return <Icons name={iconName} size={size} color={color} />;
+        },
+        headerShown: false,
+        tabBarShowLabel: false,
+      })}
+    >
       <Tab.Screen name="Home" component={Feed} />
       <Tab.Screen name="Perfil" component={Perfil} />
     </Tab.Navigator>
